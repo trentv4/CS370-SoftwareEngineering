effe31da1402a245021c81a8a5f605441c3bb3f3
trentv4 <trentvanslyke@gmail.com>
2021-09-04 06:41:27 -0400

    Add buffer system to GameState.

--- src/logic/GameLogic.cs
+++ src/logic/GameLogic.cs
@@ -14,21 +14,25 @@ namespace Project {
 	}
 
 	public class GameLogic {
+		/// <summary> Actively modified state, constantly being changed by Update() </summary>
 		private GameState State = new GameState();
+		/// <summary> "Backbuffer" of the game state, which is only ever changed in one go. This will be the state sent to the renderer. </summary>
+		private GameState StateBuffer = new GameState();
 
 		/// <summary> Handles all on-startup tasks, instantiation of objects, or other similar run-once tasks. </summary>
 		public void Initialize() {
 
 		}
 
-		/// <summary> Primary gameplay loop. </summary>
+		/// <summary> Primary gameplay loop. Make all your calls and modifications to State, not StateBuffer!</summary>
 		public void Update() {
 			Console.WriteLine("Update thread running");
+			StateBuffer = State;
 		}
 
 		/// <summary> Retrieve a copy of the GameState, typically for the renderer. </summary>
 		public GameState GetGameState() {
-			return State;
+			return StateBuffer;
 		}
 	}
 }
