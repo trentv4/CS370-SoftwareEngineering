5d0614011b916b1fba1e4097dc2819f28deeee86
trentv4 <trentvanslyke@gmail.com>
2021-09-18 04:29:39 -0400

    Add texture support to interface objects

--- src/render/shaders/InterfaceShader_fragment.glsl
+++ src/render/shaders/InterfaceShader_fragment.glsl
@@ -2,9 +2,13 @@
 
 in vec2 uv;
 
+uniform sampler2D albedoTexture;
+
 out vec4 FragColor;
 
 void main() {
-	vec4 outputColor = vec4(1.0, 0.4, 0.6, 1.0);
-	FragColor = outputColor;
+	vec4 texture = texture(albedoTexture, uv);
+	vec3 albedo = vec3(0.8, 0.2, 0.5);
+	vec3 outputColor = (texture.xyz * texture.w) + (albedo * (1-texture.w));
+	FragColor = vec4(outputColor, 1.0);
 }
