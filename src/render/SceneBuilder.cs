9d5f3041e0df226187d60c3b35c14749a8f837e0
trentv4 <trentvanslyke@gmail.com>
2021-10-19 02:20:38 -0400

    Add "rear view mirror" minimap

--- src/render/SceneBuilder.cs
+++ src/render/SceneBuilder.cs
@@ -98,8 +98,51 @@ namespace Project.Render {
 
 		public RenderableNode BuildInGameInterface(GameState state) {
 			RenderableNode node = new RenderableNode();
+			Room currentLevel = state.Level.CurrentRoom;
 			Vector2 screenSize = Renderer.INSTANCE.Size;
 			Inventory inventory = state.Level.Player.Inventory;
+
+			// Minimap "rear view mirror"
+
+			List<InterfaceModel> circles = new List<InterfaceModel>();
+			List<InterfaceModel> connectionModels = new List<InterfaceModel>();
+
+			Vector2 centerPosition = new Vector2(screenSize.X / 2, screenSize.Y - (250f / 2f));
+			circles.Add(InterfaceModel.GetCachedModel("unit_circle")
+				.SetTexture(new Texture("assets/textures/gold.png", TextureMinFilter.Nearest))
+				.SetScale(30f)
+				.SetPosition(centerPosition));
+
+			circles.Add(InterfaceModel.GetCachedModel("pointer")
+				.SetScale(30f)
+				.SetTexture(new Texture("assets/textures/black.png"))
+				.SetPosition(centerPosition)
+				.SetRotation(state.CameraYaw - 90));
+
+			foreach (Room connection in currentLevel.ConnectedRooms) {
+				float angle = currentLevel.AngleToRoom(connection);
+				Texture tex = new Texture("assets/textures/red.png");
+				if (connection == state.Level.PreviousRoom)
+					tex = new Texture("assets/textures/blue.png");
+
+				Vector2 connectionPosition = centerPosition + new Vector2((float)Math.Cos(angle) * 75, (float)Math.Sin(angle) * 75);
+				circles.Add(InterfaceModel.GetCachedModel("unit_circle")
+					.SetTexture(tex)
+					.SetScale(20f)
+					.SetPosition(connectionPosition));
+
+				Vector2 positionBNormalized = connectionPosition - centerPosition;
+				connectionModels.Add(InterfaceModel.GetCachedModel("unit_rectangle")
+					.SetPosition((connectionPosition + centerPosition) / 2)
+					.SetScale(new Vector2(Vector2.Distance(Vector2.Zero, positionBNormalized), 10f))
+					.SetRotation((float)Math.Atan2(positionBNormalized.Y, positionBNormalized.X) / Renderer.RCF));
+			}
+
+			node.Children.AddRange(new RenderableNode[] { });
+			node.Children.AddRange(connectionModels);
+			node.Children.AddRange(circles);
+
+			// Inventory wheel
 
 			int currentIndex = inventory.Position;
 			if (inventory.Items.Count > 0) {
@@ -107,11 +150,8 @@ namespace Project.Render {
 					.SetPosition(new Vector2(screenSize.X / 2, 150))
 					.SetScale(150f / 2)
 					.SetTexture(new Texture($"assets/textures/{inventory.Items[currentIndex].Definition.TextureName}"));
-
 				InterfaceString u = new InterfaceString("calibri", "[ U ]");
 				u.SetScale(35f).SetPosition(new Vector2((screenSize.X / 2) - ((u.Width / 2) * u.Scale.X), 40f));
-
-
 				node.Children.AddRange(new RenderableNode[] { currentItem, u });
 			}
 			if (inventory.Items.Count > 1) {
@@ -124,10 +164,8 @@ namespace Project.Render {
 					.SetScale(100f / 2)
 					.SetOpacity(0.5f)
 					.SetTexture(new Texture($"assets/textures/{inventory.Items[rightIndex].Definition.TextureName}"));
-
 				InterfaceString spinRight = new InterfaceString("calibri", "[E]");
 				spinRight.SetScale(35f).SetPosition(new Vector2((screenSize.X / 2) - ((spinRight.Width / 2) * spinRight.Scale.X) + 150, 165));
-
 				node.Children.AddRange(new RenderableNode[] { rightItem, spinRight });
 			}
 
@@ -140,10 +178,8 @@ namespace Project.Render {
 					.SetScale(100f / 2)
 					.SetOpacity(0.5f)
 					.SetTexture(new Texture($"assets/textures/{inventory.Items[leftIndex].Definition.TextureName}"));
-
 				InterfaceString spinLeft = new InterfaceString("calibri", "[Q]");
 				spinLeft.SetScale(35f).SetPosition(new Vector2((screenSize.X / 2) - ((spinLeft.Width / 2) * spinLeft.Scale.X) - 150, 165));
-
 				node.Children.AddRange(new RenderableNode[] { leftItem, spinLeft });
 			}
 
