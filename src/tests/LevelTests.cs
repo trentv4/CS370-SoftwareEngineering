0fae5ea11a35883e089e24f81892918154fac2e0
moneyl <8206401+Moneyl@users.noreply.github.com>
2021-11-08 18:42:34 -0500

    Fix level gen unit test

--- src/tests/LevelTests.cs
+++ src/tests/LevelTests.cs
@@ -8,11 +8,11 @@ namespace Project.Tests {
     public class LevelTests {
         [Test]
         public void TestRoomValidity() {
-            //Create new level. Constructor randomly generates a set of rooms and connections
-            var level = new Level();
-            
-            //Ensure that all rooms have at least 2 connections so the player can't get stuck
-            foreach (var room in level.Rooms) {
+			//Generate a new level
+			Level level = LevelGenerator.TryGenerateLevel();
+
+			//Ensure that all rooms have at least 2 connections so the player can't get stuck
+			foreach (var room in level.Rooms) {
                 Assert.IsTrue(room.ConnectedRooms.Length >= 2);
             }
         }
